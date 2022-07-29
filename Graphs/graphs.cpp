191ca567d5004dbad3f03da34050ff4615058705
Noob169 <boney0310@gmail.com>
2022-07-29 19:14:07 +0530

    added comments

--- Graphs/graphs.cpp
+++ Graphs/graphs.cpp
@@ -1,15 +1,19 @@
+//IMPLEMENTING A GRAPH USING ADJACENCY LIST
 #include <bits/stdc++.h>
 using namespace std;
-//IMPLEMENTING ADJACENCY LIST REP OF A GRAPH
 
-template <typename T>
-void printGraph(map<T,vector<T>>&graph, T null_value=-1)
+
+//printGraph(<graph-name>,<null-value>) is used for printing any graph, where null-value is the value which keys having no values 
+//are assigned to. Used for checking purposes. The default null-value is -1.
+template <typename T>           
+void printGraph(map<T,vector<T>>&graph, T null_value=-1)        
 {
     cout<<"The graph:-"<<endl;
-    for(typename map<T,vector<T>>::iterator key=graph.begin();key!=graph.end();key++)
+    for(typename map<T,vector<T>>::iterator key=graph.begin();key!=graph.end();key++)   //typename is required when declaring an 
+    //iterator using a template variable
     {
         cout<<"["<<key->first<<"]"<<"--->";
-        if(key->second[0]==null_value)
+        if(key->second[0]==null_value)      
         {
             cout<<"None"<<endl;
             continue;
@@ -20,15 +24,19 @@ void printGraph(map<T,vector<T>>&graph, T null_value=-1)
     }
 }
 
-template <typename T>
+/*BFS implementation. BFS(<vector-name>,<null-value>) is used for traversing any graph, 
+where null-value is the value which keys having no values have.*/
+template <typename T>                          
 void BFS(map<T,vector<T>>&graph, T null_value)
 {
-    if(graph.size()==0)
+     //redeclaration of templates is required for using templates in more than one functions
+    if(graph.size()==0)     //if graph has no keys
     return;
     cout<<"The BFS results:"<<endl;
     T key;
     typename map<T,vector<T>>::iterator itr=graph.begin();
-    vector<T>vis={itr->first},queue={itr->first};
+    vector<T>vis={itr->first},queue={itr->first};       //vis is used for storing the visited nodes, queue is used 
+    //for storing the nodes to be visited
     typename vector<T>::iterator val_itr;
     while(!queue.empty())
     {
@@ -36,11 +44,13 @@ void BFS(map<T,vector<T>>&graph, T null_value)
         key=*val_itr;
         queue.erase(val_itr);
         cout<<key<<" ";
-        if(graph[key].at(0)!=null_value)
+        if(graph[key].at(0)!=null_value)                //if key has values
         {
             for(val_itr=graph[key].begin();val_itr!=graph[key].end();val_itr++)
             {
-                if(find(vis.begin(),vis.end(),*val_itr)==vis.end())
+                if(find(vis.begin(),vis.end(),*val_itr)==vis.end())         //find(<vector>.begin,<vector>.end,item_to_be_Searched) 
+                //is used for checking if the value is already present in the vector. If not present, it returns the end() iterator.
+                //else it returns a reference to the position where the item is
                 {
                     queue.push_back(*val_itr);
                     vis.push_back(*val_itr);
